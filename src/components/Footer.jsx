9a39d62ec5ce8bdd04c45cb670e02a877137c2d2
Mithilesh-123 <mithileshmahatha8789@gmail.com>
2022-11-23 22:09:35 +0530

    Created a normal webiste for practice

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/footer.scss'
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href='https://www.linkedin.com/in/mithilesh-kumar-mahatha-2b3a99195/' target={"_blank"}>LinkedIn</a>
+                <a href=''  target={"_blank"}>Instagram</a>
+                <a href='https://github.com/Mithilesh-123'  target={"_blank"}>GitHub</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
